102c4e24a3f1519293c8ad3f141ec22637b50e44
Fasa47 <sandor.farkas.fasa47@gmail.com>
2021-08-06 16:30:11 +0200

    Initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MarketigApp from './components/MarketingApp'
+
+export default () => {
+    return (
+        <div>
+            <h1>Hi there!</h1>
+            <hr />
+            <MarketigApp />
+        </div>
+    )
+}
